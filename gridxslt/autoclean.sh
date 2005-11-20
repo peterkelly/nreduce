d59977bdf86d6de0951e0f52d167b1fa018cb821
Peter Kelly <peter@pmkelly.net>
2005-11-20 12:02:39 +0000

    Run "make distclean" first if necesary

--- gridxslt/autoclean.sh
+++ gridxslt/autoclean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-make distclean
+if [ -f Makefile ]; then
+  make distclean
+fi
 rm -rf autom4te.cache
 rm -f config.guess config.sub configure depcomp install-sh ltmain.sh Makefile.in missing aclocal.m4
 rm -f */Makefile.in
