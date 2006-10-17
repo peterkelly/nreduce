342c6f61ea5cd87675a456c0456ea7e5f31603fd
Peter Kelly <peter@pmkelly.net>
2006-10-17 07:15:20 +0000

    Initial import of showload and parsh

---
+++ tools/parsh.c
@@ -0,0 +1,247 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+
+#define MAXHOSTS 1024
+
+struct buffer {
+  int size;
+  int allocated;
+  char *data;
+};
+
+void buffer_append(struct buffer *buf, const char *data, int size)
+{
+  while (buf->size+size >= buf->allocated) {
+    buf->allocated += 1024;
+    buf->data = (char*)realloc(buf->data,buf->allocated);
+  }
+  memcpy(buf->data+buf->size,data,size);
+  buf->size += size;
+}
+
+int outfds[MAXHOSTS];
+pid_t pids[MAXHOSTS];
+struct buffer hostbufs[MAXHOSTS];
+char *hostnames[MAXHOSTS];
+int nhosts = 0;
+
+struct arguments {
+  char *hosts;
+  char *username;
+  char *shell;
+  int oneline;
+  char *cmd;
+  char **args;
+  int nargs;
+};
+
+struct arguments arguments;
+
+void usage()
+{
+  printf(
+"Usage: parsh -h HOSTFILE [OPTIONS] COMMAND\n"
+"\n"
+"  -h, --hosts FILE         Read host list from FILE\n"
+"  -s, --shell SHELL        Use SHELL instead of rsh\n"
+"  -o, --oneline            Print each result on one line\n"
+"\n"
+"If either of -h or -s is missing, the values will be taken\n"
+"from $PARSH_HOSTS and $PARSH_SHELL, respectively. The default\n"
+"shell is rsh.\n");
+  exit(1);
+}
+
+void parse_args(int argc, char **argv)
+{
+  int i;
+  if (1 >= argc)
+    usage();
+
+  for (i = 1; i < argc; i++) {
+    if (!strcmp(argv[i],"-h") || !strcmp(argv[i],"--hosts")) {
+      if (++i >= argc)
+        usage();
+      arguments.hosts = argv[i];
+    }
+    else if (!strcmp(argv[i],"-s") || !strcmp(argv[i],"--shell")) {
+      if (++i >= argc)
+        usage();
+      arguments.shell = argv[i];
+    }
+    else if (!strcmp(argv[i],"-o") || !strcmp(argv[i],"--oneline")) {
+      arguments.oneline = 1;
+    }
+    else {
+      break;
+    }
+  }
+
+  arguments.args = &argv[i];
+  arguments.nargs = argc-i;
+  if ((0 >= arguments.nargs) || (NULL == arguments.hosts))
+    usage();
+}
+
+void run_shell(char *host, int argc, char **argv)
+{
+  int fds[2];
+  pid_t pid;
+
+  if (0 > pipe(fds)) {
+    perror("pipe");
+    exit(1);
+  }
+
+  if (0 > (pid = fork())) {
+    perror("fork");
+    exit(1);
+  }
+  else if (0 == pid) {
+    /* child */
+    char **args = (char**)malloc((3+argc)*sizeof(char*));
+    int argno = 0;
+    int i;
+
+    args[argno++] = arguments.shell;
+    args[argno++] = host;
+    for (i = 0; i < argc; i++)
+      args[argno++] = argv[i];
+    args[argno++] = NULL;
+
+    dup2(fds[1],STDOUT_FILENO);
+    dup2(fds[1],STDERR_FILENO);
+    close(fds[0]);
+
+    execvp(args[0],args);
+    perror(args[0]);
+    exit(-1);
+  }
+  else {
+    /* parent */
+    close(fds[1]);
+
+    outfds[nhosts] = fds[0];
+    pids[nhosts] = pid;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  char *host;
+  FILE *hf;
+  int i;
+
+  memset(&arguments,0,sizeof(struct arguments));
+
+  setbuf(stdout,NULL);
+
+  arguments.hosts = getenv("PARSH_HOSTS");
+  arguments.shell = getenv("PARSH_SHELL");
+  if (NULL == arguments.shell)
+    arguments.shell = "rsh";
+
+  parse_args(argc,argv);
+
+  memset(hostbufs,0,sizeof(MAXHOSTS*sizeof(struct buffer)));
+  memset(hostnames,0,sizeof(MAXHOSTS*sizeof(struct buffer)));
+
+  if (NULL == (hf = fopen(arguments.hosts,"r"))) {
+    perror(arguments.hosts);
+    exit(1);
+  }
+
+  while ((1 == fscanf(hf,"%as",&host)) && (MAXHOSTS > nhosts)) {
+    hostnames[nhosts] = host;
+    run_shell(host,arguments.nargs,arguments.args);
+    fcntl(outfds[nhosts],F_SETFL,fcntl(outfds[nhosts],F_GETFL)|O_NONBLOCK);
+    nhosts++;
+  }
+
+  while (1) {
+    fd_set readfds;
+    fd_set exceptfds;
+    int highest = 0;
+    int found = 0;
+    int r;
+    struct timeval tv = { tv_sec: 5, tv_usec: 0 };
+
+    FD_ZERO(&readfds);
+    FD_ZERO(&exceptfds);
+    for (i = 0; i < nhosts; i++) {
+      if (0 > outfds[i])
+        continue;
+      FD_SET(outfds[i],&readfds);
+      FD_SET(outfds[i],&exceptfds);
+      if (highest < outfds[i])
+        highest = outfds[i];
+      found++;
+    }
+
+    if (!found)
+      break;
+
+    if (0 > (r = select(highest+1,&readfds,NULL,&exceptfds,&tv))) {
+      perror("select");
+      exit(1);
+    }
+
+    if (0 == r) {
+      printf("Waiting on:\n");
+      for (i = 0; i < nhosts; i++) {
+        if (0 <= outfds[i])
+          printf("  %s\n",hostnames[i]);
+      }
+    }
+
+    for (i = 0; i < nhosts; i++) {
+      if (0 > outfds[i])
+        continue;
+      if (FD_ISSET(outfds[i],&readfds) || FD_ISSET(outfds[i],&exceptfds)) {
+        char tmpbuf[1024];
+        while (0 < (r = read(outfds[i],tmpbuf,1024))) {
+          buffer_append(&hostbufs[i],tmpbuf,r);
+        }
+        if ((0 != r) && (EAGAIN != errno)) {
+          printf("%s: error: %s\n",hostnames[i],strerror(errno));
+          close(outfds[i]);
+          outfds[i] = -1;
+        }
+        if (0 == r) {
+          char zero = '\0';
+          buffer_append(&hostbufs[i],&zero,1);
+
+          close(outfds[i]);
+          outfds[i] = -1;
+
+          if (arguments.oneline) {
+            int c;
+            for (c = 0; c < hostbufs[i].size; c++) {
+              if ('\n' == hostbufs[i].data[c])
+                hostbufs[i].data[c] = ' ';
+            }
+            printf("%-30s %s",hostnames[i],hostbufs[i].data);
+            printf("\n");
+          }
+          else {
+            printf("=============== %s ===============\n",hostnames[i]);
+            if (NULL != hostbufs[i].data)
+              printf("%s",hostbufs[i].data);
+          }
+
+        }
+      }
+    }
+  }
+
+  fclose(hf);
+
+  return 0;
+}
---
+++ tools/showload.c
@@ -0,0 +1,389 @@
+#define _GNU_SOURCE /* for asprintf */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+#include <curses.h>
+#include <signal.h>
+
+#define CHUNKSIZE 4
+#define LOGFILENAME "clustermon.log"
+#define DELAYMS 1000
+#define HOSTWIDTH 12
+
+FILE *logfile = NULL;
+int startsec = 0;
+int rows = 0;
+int cols = 0;
+
+typedef struct host {
+  char *hostname;
+  int pid;
+  int fd;
+  int count;
+} host;
+
+char *readfile(FILE *f)
+{
+  char buf[CHUNKSIZE];
+  char *data = NULL;
+  int size = 0;
+  int r;
+  while (0 < (r = fread(buf,1,CHUNKSIZE,f))) {
+    data = (char*)realloc(data,size+r+1);
+    memcpy(&data[size],buf,r);
+    size += r;
+  }
+  data[size++] = '\0';
+  return data;
+}
+
+char *readfd(int fd)
+{
+  char buf[CHUNKSIZE];
+  char *data = NULL;
+  int size = 0;
+  int r;
+  while (0 < (r = read(fd,buf,CHUNKSIZE))) {
+    data = (char*)realloc(data,size+r+1);
+    memcpy(&data[size],buf,r);
+    size += r;
+  }
+  data[size++] = '\0';
+  return data;
+}
+
+char *read_hosts(const char *filename)
+{
+  char *data;
+  int fd;
+
+  if (0 > (fd = open(filename,O_RDONLY))) {
+    perror(filename);
+    exit(-1);
+  }
+
+  data = readfd(fd);
+  close(fd);
+  return data;
+}
+
+int parse_hosts(const char *data, host **hout)
+{
+  int nhosts;
+  host *hosts;
+  int start;
+  int pos;
+  int h;
+
+  nhosts = 0;
+  start = 0;
+  for (pos = 0; '\0' != data[pos]; pos++) {
+    if (('\n' == data[pos]) || ('\0' == data[pos])) {
+      if (0 < pos-start)
+        nhosts++;
+      start = pos+1;
+    }
+  }
+
+  hosts = (host*)calloc(nhosts,sizeof(host));
+  h = 0;
+  start = 0;
+  for (pos = 0; '\0' != data[pos]; pos++) {
+    if (('\n' == data[pos]) || ('\0' == data[pos])) {
+      if (0 < pos-start) {
+        hosts[h].hostname = (char*)malloc(pos-start+1);
+        memcpy(hosts[h].hostname,&data[start],pos-start);
+        hosts[h].hostname[pos-start] = '\0';
+        h++;
+      }
+      start = pos+1;
+    }
+  }
+
+  *hout = hosts;
+  return nhosts;
+}
+
+int run_process(pid_t *pid, ...)
+{
+  va_list ap;
+  int nargs = 0;
+  int fds[2];
+
+  va_start(ap,pid);
+  while (NULL != va_arg(ap,char*))
+    nargs++;
+  va_end(ap);
+
+  if (0 > pipe(fds)) {
+    perror("pipe");
+    exit(-1);
+  }
+
+  *pid = fork();
+  if (0 > *pid) {
+    /* fork failed */
+    perror("fork");
+    exit(-1);
+  }
+  else if (0 == *pid) {
+    /* child */
+    char **args = (char**)malloc((nargs+1)*sizeof(char*));
+    int argno = 0;
+
+    va_start(ap,pid);
+    for (argno = 0; argno < nargs; argno++)
+      args[argno] = va_arg(ap,char*);
+    va_end(ap);
+
+    args[argno] = NULL;
+
+    dup2(fds[1],STDOUT_FILENO);
+    dup2(fds[1],STDERR_FILENO);
+    close(fds[0]);
+
+    execvp(args[0],args);
+    printf("Can't execute %s: %s\n",args[0],strerror(errno));
+    exit(-1);
+  }
+  else {
+    /* parent */
+    close(fds[1]);
+    return fds[0];
+  }
+  return -1;
+}
+
+void login_hosts(host *hosts, int nhosts, const char *cmd)
+{
+  char dir[PATH_MAX];
+  int h;
+
+  if (NULL == getcwd(dir,PATH_MAX)) {
+    perror("getcwd");
+    exit(-1);
+  }
+
+  for (h = 0; h < nhosts; h++) {
+    char *rshcmd;
+    asprintf(&rshcmd,"cd %s && %s SLAVE",dir,cmd);
+    hosts[h].fd = run_process(&hosts[h].pid,"rsh",hosts[h].hostname,rshcmd,NULL);
+  }
+}
+
+void finish(int sig)
+{
+  endwin();
+  fclose(logfile);
+  exit(0);
+}
+
+void update(host *hosts, int nhosts, int h, int load)
+{
+  int i;
+  double ratio = ((double)(cols-HOSTWIDTH))/100.0;
+  int nchars = (int)(((double)load)*ratio);
+  struct timeval now;
+
+  gettimeofday(&now,NULL);
+
+  mvaddstr(h+2,0,hosts[h].hostname);
+  if (0 < hosts[h].count++) {
+
+    if (0 > load) {
+      mvaddstr(h+2,HOSTWIDTH,"error");
+      fprintf(logfile,"%s %d.%03d error\n",
+              hosts[h].hostname,(int)now.tv_sec-startsec,(int)now.tv_usec/1000);
+    }
+    else {
+      for (i = 0; i < cols-HOSTWIDTH; i++) {
+        if (i < nchars) {
+          attron(A_REVERSE);
+        }
+        else {
+          attroff(A_REVERSE);
+        }
+        mvaddstr(h+2,HOSTWIDTH+i," ");
+      }
+      attroff(A_REVERSE);
+      fprintf(logfile,"%s %d.%03d %d\n",
+              hosts[h].hostname,(int)now.tv_sec-startsec,(int)now.tv_usec/1000,load);
+    }
+  }
+
+  refresh();
+}
+
+void loop(host *hosts, int nhosts)
+{
+  while (1) {
+    fd_set rfds;
+    int h;
+    int highest = -1;
+    int s;
+
+    FD_ZERO(&rfds);
+    for (h = 0; h < nhosts; h++) {
+      FD_SET(hosts[h].fd,&rfds);
+      if ((0 > highest) || (highest < hosts[h].fd))
+        highest = hosts[h].fd;
+    }
+
+    while ((0 > (s = select(highest+1,&rfds,NULL,NULL,NULL))) &&
+           (EINTR == errno));
+
+    if (0 > s) {
+      perror("select");
+      exit(-1);
+    }
+
+    for (h = 0; h < nhosts; h++) {
+      if (FD_ISSET(hosts[h].fd,&rfds)) {
+        char c;
+        int r;
+        while ((0 > (r = read(hosts[h].fd,&c,1))) &&
+               (EINTR == errno));
+        if (0 > r) {
+          fprintf(stderr,"read() from %s: %s\n",hosts[h].hostname,strerror(errno));
+          exit(-1);
+        }
+        if (0 == r) {
+          fprintf(stderr,"read() from %s returned 0 bytes\n",hosts[h].hostname);
+          exit(-1);
+        }
+        update(hosts,nhosts,h,c);
+      }
+    }
+  }
+}
+
+
+int getload()
+{
+  static int oldtotal = -1;
+  static int oldidle = -1;
+
+  FILE *f;
+  int count;
+  int user = 0;
+  int nice = 0;
+  int system = 0;
+  int idle = 0;
+  int iowait = 0;
+  int irq = 0;
+  int softirq = 0;
+  int total;
+  int totaldiff;
+  int idlediff;
+
+  if (NULL == (f = fopen("/proc/stat","r")))
+    return -1;
+
+  count = fscanf(f,"cpu %d %d %d %d %d %d %d",&user,&nice,&system,&idle,&iowait,&irq,&softirq);
+  fclose(f);
+
+  if ((4 != count) && (7 != count))
+    return -1;
+
+  total = user+nice+system+idle+iowait+irq+softirq;
+
+  if (0 > total) {
+    oldtotal = total;
+    oldidle = idle;
+    return -1;
+  }
+
+  totaldiff = total-oldtotal;
+  idlediff = idle-oldidle;
+
+  oldtotal = total;
+  oldidle = idle;
+
+  if (0 == totaldiff)
+    return -1;
+
+  return 100-100*idlediff/totaldiff;
+}
+
+void slave()
+{
+  while (1) {
+    int load = getload();
+    char c = (char)load;
+    struct timespec time;
+
+    write(STDOUT_FILENO,&c,1);
+
+    time.tv_sec = DELAYMS/1000;
+    time.tv_nsec = (DELAYMS%1000)*1000000;
+    nanosleep(&time,NULL);
+
+    load++;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  char *data;
+  int nhosts;
+  host *hosts;
+  struct timeval now;
+
+  setbuf(stdout,NULL);
+
+  if (2 > argc) {
+    fprintf(stderr,"Usage: clustermon <hosts>\n");
+    return -1;
+  }
+
+  if (!strcmp("SLAVE",argv[1])) {
+    slave();
+    return 0;
+  }
+
+  if (NULL == (logfile = fopen(LOGFILENAME,"w"))) {
+    perror(LOGFILENAME);
+    return -1;
+  }
+  setbuf(logfile,NULL);
+
+  gettimeofday(&now,NULL);
+  startsec = now.tv_sec;
+
+  signal(SIGINT,finish);
+
+  initscr();
+  keypad(stdscr,1);
+  nonl();
+  cbreak();
+  noecho();
+  curs_set(0);
+  getmaxyx(stdscr,rows,cols);
+
+  mvaddstr(0,0,"Host");
+  mvaddstr(0,HOSTWIDTH,"0");
+  mvaddstr(0,HOSTWIDTH+(cols-HOSTWIDTH)/2-1,"50");
+  mvaddstr(0,cols-4,"100");
+
+  refresh();
+
+  data = read_hosts(argv[1]);
+  nhosts = parse_hosts(data,&hosts);
+  login_hosts(hosts,nhosts,argv[0]);
+  loop(hosts,nhosts);
+
+  finish(0);
+
+  return 0;
+}
