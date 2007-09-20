8d44c3818559e5816fcddfa64cd91fe7a18e7bb9
Peter Kelly <peter@pmkelly.net>
2007-09-20 04:34:07 +0000

    Added an example web sevice using JAX-WS

---
+++ webservices/jaxws2/client/Client.java
@@ -0,0 +1,50 @@
+package client;
+
+import java.net.URL;
+import java.util.List;
+import javax.xml.namespace.QName;
+
+import proxy.Test2;
+import proxy.Test2Service;
+import proxy.Person;
+
+public class Client
+{
+    public static void main(String[] args)
+        throws Exception
+    {
+        if (args.length < 1) {
+            System.err.println("Usage: Client <wsdl-url>\n");
+            System.exit(-1);
+        }
+
+
+        Test2Service locator = new Test2Service(new URL(args[0]),
+                                                new QName("http://test2/", "Test2Service"));
+        Test2 svc = locator.getTest2Port();
+
+        System.out.println("Names:\n");
+        List<String> names = svc.getNames();
+        for (int i = 0; i < names.size(); i++)
+            System.out.println(names.get(i));
+        System.out.println();
+
+        System.out.println("Individual people:\n");
+        for (String name : names) {
+            Person p = svc.getPerson(name);
+            System.out.println("name = "+p.getName()+
+                               ", age = "+p.getAge()+
+                               ", occupation = "+p.getOccupation());
+        }
+        System.out.println();
+
+        System.out.println("All people:\n");
+        List<Person> people = svc.getAllPeople();
+        for (Person person : people) {
+            System.out.println("name = "+person.getName()+
+                               ", age = "+person.getAge()+
+                               ", occupation = "+person.getOccupation());
+        }
+    }
+
+}
